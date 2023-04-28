bf95d4b38cf61968928a2b3c11f99b13e948dba6
Olli365 <ofstoole@gmail.com>
2023-04-28 12:34:07 +0100

    moved all powerups into powerUpRandom

--- core/src/com/team13/piazzapanic/PlayScreen.java
+++ core/src/com/team13/piazzapanic/PlayScreen.java
@@ -35,10 +35,7 @@ import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import powerUps.cookingSpeedBoost;
-import powerUps.speedUpCooking;
-import powerUps.addLife;
-import powerUps.addOrderTimer;
-import powerUps.runSpeedUp;
+import powerUps.powerUpRandom;
 
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
@@ -182,7 +179,7 @@ public class PlayScreen implements Playable {
         world = new World(new Vector2(0,0), true);
         new B2WorldCreator(world, map, this);
         powerUp  = new cookingSpeedBoost(this.world,new TextureRegion( new  Texture("powerUps/powerUpCoin.png")), 126,85);
-        powerUp.setPowerUp(new runSpeedUp());
+        powerUp.setPowerUp(new powerUpRandom());
         chefList.add(new Chef(this, 31.5F,65));
         chefList.add(new Chef(this, 128,65));
         chefList.add(new Chef(this, 128, 88));
@@ -479,7 +476,7 @@ public class PlayScreen implements Playable {
         if (TimeUtils.timeSinceMillis(spawnNewPowerUpTimer) > timeToNewPower){
             // Ths if statement controls when a new powerup will spawn. they spawn a regular intervals defined by a timer
             cookingSpeedBoost newPower =  new cookingSpeedBoost(this.world,new TextureRegion( new  Texture("powerUps/powerUpCoin.png")), 0.4f,0.4f);
-            newPower.setPowerUp(new runSpeedUp());
+            newPower.setPowerUp(new powerUpRandom());
             powerUpArray.add(newPower);
             spawnNewPowerUpTimer = TimeUtils.millis();
 
---
+++ core/src/powerUps/powerUpRandom.java
@@ -0,0 +1,30 @@
+package powerUps;
+
+import Sprites.Chef;
+import Sprites.OrderTimer;
+import java.util.Random;
+
+public class powerUpRandom extends powerUpGeneric{
+    private Random randomGenerator = new Random();
+    @Override
+    public void improveChef(Chef chefToModify, OrderTimer orderTimer){
+        int randNumber = randomGenerator.nextInt(4);
+        switch(randNumber){
+            case 1:
+                chefToModify.setRunSpeedModifier(1.5F);
+                break;
+            case 2:
+                chefToModify.setCookingSpeedModifier(2);
+                break;
+            case 3:
+                orderTimer.stopTimer((1));
+                break;
+            case 4:
+                chefToModify.add1Life();
+                break;
+
+        }
+
+        //
+    }
+}
